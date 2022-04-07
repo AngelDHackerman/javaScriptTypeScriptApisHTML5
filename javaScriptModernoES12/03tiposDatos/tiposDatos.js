55613f2911e437dd77178755c6e3bcb6c7e635b6
angelDariaux <angeldariaux@gmail.com>
2022-04-07 16:43:23 -0600

    primeros tipos de datos

---
+++ javaScriptModernoES12/03tiposDatos/tiposDatos.js
@@ -0,0 +1,59 @@
+      // String 
+
+let nombre = 'Angel Hernandez';
+
+      // Number
+
+let numero = 14; 
+let decimal = 3.2;
+
+      //  Boolean 
+
+let mayorEdad = true;
+let menorEdad = Boolean(0); // * 1 es true, 0 es false
+
+console.group('String, number, boolean:')
+
+  console.log(
+    typeof nombre,
+    typeof numero,
+    typeof decimal,
+    typeof mayorEdad,
+    typeof menorEdad
+  )
+
+console.groupEnd()
+
+
+      // Array
+      
+let paises = ['España', 'Mexico', 'Colombia', 'Argentina'];
+
+      // Null
+
+let vacio = null;
+
+      // Undefined
+
+let noDefinido = undefined;
+
+
+      // JSON - Javascript object Notation - Objetos literales
+
+      // Symbol
+
+
+
+console.group('Array, Null, Undefined:');
+
+    console.log(
+      typeof(paises),
+      typeof(vacio),
+      typeof(noDefinido)
+    );
+
+console.groupEnd();
+
+
+
+
