ae5377e2456c00f39e59935de791cfca3af93826
angelDariaux <angeldariaux@gmail.com>
2022-04-06 20:26:32 -0600

    variables con var y let

---
+++ javaScriptModernoES12/01Variables/js/01Variables.js
@@ -0,0 +1,43 @@
+// * EcmaScript 2021 / ES12 / ES.NEXT
+
+        // ? Var 
+
+console.group('Var:')
+
+var nombre = 'Angel Hernandez Web';
+
+const monstrarNombre = () => {
+  var nombre = 'pepito';
+  var apellido = 'botica';
+  console.log('dentro:', nombre)
+  console.log('dentro:', apellido)
+
+
+}
+monstrarNombre();
+console.log('fuera:', nombre)
+
+console.groupEnd();
+
+
+      // ? let 
+
+console.group('Let:')
+
+let pais = 'Guatemala';
+
+const mostrarPais = () => {
+  console.log(pais)
+}
+mostrarPais();
+
+if (pais == 'Guatemala') { 
+  let continente = 'America';
+  console.log(continente)
+}
+
+for (let contador = 1; contador <= 4; contador++) {
+  console.log(contador)
+}
+
+console.groupEnd();
