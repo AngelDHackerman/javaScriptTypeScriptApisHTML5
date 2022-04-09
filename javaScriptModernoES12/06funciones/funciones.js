df4a18af81a5ee7ff0ac2da32d1fec57f41d729a
angelDariaux <angeldariaux@gmail.com>
2022-04-08 22:10:53 -0600

    funciones con parametros por defecto

---
+++ javaScriptModernoES12/06funciones/funciones.js
@@ -0,0 +1,5 @@
+function saludo (persona = 'Angel Hernandez', year = 2022) { // ? = 'Angel Hernandez' es un parametro opcional por si no se los pasan cuando se llama la funcion
+  console.log(`Hola, te mando un saludo ${persona} estamos a ${year}`)
+}
+
+saludo('Pepillo', 2030);
