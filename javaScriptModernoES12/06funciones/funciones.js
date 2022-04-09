1ee46d81a37837c436bbccf167bd7fbebf33914e
angelDariaux <angeldariaux@gmail.com>
2022-04-08 22:37:30 -0600

    arrow functions y sus limitaciones como metodos y usando el this

--- javaScriptModernoES12/06funciones/funciones.js
+++ javaScriptModernoES12/06funciones/funciones.js
@@ -7,6 +7,7 @@ function saludo (persona = 'Angel Hernandez', year = 2022) { // ? = 'Angel Herna
 saludo('Pepillo', 2030);
 
 
+
       // ? Funciones flecha.
 
 let saludo2 = nombre => `Hola te mando un saludo ${nombre}!`;
@@ -24,3 +25,26 @@ const edad = (nombre, edad) => {
 }
 
 edad('angel', 26)
+
+
+
+      // ? Arrow funcion y JSON 
+
+let tienda = { 
+  nombre: 'Game',
+  videojuegos: ['GTA', 'WWE', 'FIFA'],
+  mostrar: function() { 
+    console.log(this.nombre);
+  },
+  mostrar2() { 
+    this.videojuegos.forEach(juego => console.log(juego));
+    
+  },
+  mostrar3: () => { 
+    console.log(this.nombre); // ! this. eso no es aconsejable, NO se debe usar como metodo.
+  }
+}
+
+tienda.mostrar();
+tienda.mostrar2();
+tienda.mostrar3(); // ! Aqui da undefined porque en arrow functions NO existe el objeto this.
