f635afbf0d5267ea798b02eda5bfa5ce27103553
angelDariaux <angeldariaux@gmail.com>
2022-04-08 22:52:22 -0600

    desestructuracion primeros pasos

---
+++ javaScriptModernoES12/07Desestructuracion/desestructuracion.js
@@ -0,0 +1,18 @@
+
+// ? La desestructuracion en javaScript nos permite extraer datos de objetos, arrays de strings.
+// ? y convertirlos a variables cada uno de esos datos, hace que esto sea mas facil que de manera clasica.
+
+let frutas = ['manzana', 'pera', 'naranja', 'sandia'];
+
+      // * clasico
+
+let manzana = frutas[0];
+let pera = frutas[1];
+
+console.log(manzana, pera);
+
+
+
+      // * Desestructuracion
+
+
