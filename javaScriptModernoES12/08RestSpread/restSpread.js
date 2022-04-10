8247eb7ac34f2dfb9abe1641263601a390e507a7
angelDariaux <angeldariaux@gmail.com>
2022-04-09 22:31:52 -0600

    usando el Rest operator

--- javaScriptModernoES12/08RestSpread/restSpread.js
+++ javaScriptModernoES12/08RestSpread/restSpread.js
@@ -1,5 +1,6 @@
       // * Spread operator
 
+console.group('Spread operator:')
 let heroes = ['Batman', 'Spiderman', 'Superman', 'Venom', 'Aquaman'];
 
 console.log(...heroes); // ? Con el spread ... hace console.log de todos los elementos dentro del array.
@@ -22,3 +23,28 @@ let superHeroes = ['Flash', 'Catwoman', ...heroes];
 console.log(...superHeroes)
 monstrarHeroes(...superHeroes);
 
+console.groupEnd()
+
+      // * REST 
+
+console.group('Rest:')
+
+const peliculas = (pelicula1, pelicula2, ...restoDePeliculas) => { 
+  console.log(pelicula1);
+  console.log(pelicula2);
+  for (pelicula of restoDePeliculas) { 
+    console.log(pelicula)
+  }
+}
+
+peliculas('El señor de los anillos',
+          'Fast and furious',
+          'Batman vs Superman',
+          'Matrix',
+          'Depredador',
+          'Terminator'
+)
+
+
+
+console.groupEnd()
