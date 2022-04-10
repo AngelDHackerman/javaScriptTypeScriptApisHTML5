305f0a8b481048b0ee58f0e8de08ada595732208
angelDariaux <angeldariaux@gmail.com>
2022-04-10 16:21:02 -0600

    importando y exportando modulos de js

---
+++ javaScriptModernoES12/12Modulos/miModulo.js
@@ -0,0 +1,10 @@
+const imprimirArray = (datos) => { 
+  datos.forEach(element => {
+    console.log(element)
+  });
+
+  return datos;
+}
+
+export default imprimirArray;   // ? Asi se exportan las funciones de este modulo
+
---
+++ javaScriptModernoES12/12Modulos/modulos.js
@@ -0,0 +1,16 @@
+// ! Se debe poner: type="module" en el HTML para que esto funcione, igual tiene que estar corriendo en un servidor local
+
+import imprimirArray from './miModulo.js';   //  ? Asi se importan los modulos
+
+
+
+let nombres = ['Angel', 'Victor', 'Paco', 'Pepe'];
+
+imprimirArray(nombres)
+
+
+
+
+
+
+
