a8e62a79b81e989a6b8346162ececd52ec3f837f
angelDariaux <angeldariaux@gmail.com>
2022-04-15 13:26:39 -0600

    objetos en TS

---
+++ typeScriptModerno/dist/05Objetos.js
@@ -0,0 +1,11 @@
+"use strict";
+let empresa = {
+    nombre: 'MICROSOFT INC',
+    sector: 'Informatica',
+    servicios: ['sistemas operativos', 'ofimatica', 'consolas'],
+    facturacion: 55000000000000
+};
+// empresa = {        
+//   nombre: 1234,      // todo: Esto no se puede hacer porque 1, no se respetal el tipo de valor y 2 no se respeta el 'key' del segundo valor
+//   categoria: 'hola'
+// }
---
+++ typeScriptModerno/src/05Objetos.ts
@@ -0,0 +1,11 @@
+let empresa = { 
+  nombre: 'MICROSOFT INC',
+  sector: 'Informatica',
+  servicios: ['sistemas operativos', 'ofimatica', 'consolas'],
+  facturacion: 55_000_000_000_000
+}
+
+// empresa = {        
+//   nombre: 1234,      // todo: Esto no se puede hacer porque 1, no se respetal el tipo de valor y 2 no se respeta el 'key' del segundo valor
+//   categoria: 'hola'
+// }
