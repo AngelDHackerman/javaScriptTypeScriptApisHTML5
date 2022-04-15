9a14ef3f7d7f8494d90ca8c1bfade54c5d6d7b12
angelDariaux <angeldariaux@gmail.com>
2022-04-15 13:35:00 -0600

    objetos en TS y declarando sus tipos de valores en un alias

--- typeScriptModerno/dist/05Objetos.js
+++ typeScriptModerno/dist/05Objetos.js
@@ -1,11 +1,18 @@
 "use strict";
+// ? Creando objetos en TS
 let empresa = {
     nombre: 'MICROSOFT INC',
     sector: 'Informatica',
     servicios: ['sistemas operativos', 'ofimatica', 'consolas'],
     facturacion: 55000000000000
 };
-// empresa = {        
-//   nombre: 1234,      // todo: Esto no se puede hacer porque 1, no se respetal el tipo de valor y 2 no se respeta el 'key' del segundo valor
-//   categoria: 'hola'
-// }
+let empresa2 = {
+    nombre: 'MICROSOFT INC',
+    sector: 'Informatica',
+    servicios: ['sistemas operativos', 'ofimatica', 'consolas'],
+    facturacion: 55000000000000,
+    mostrar() {
+        return `${this.nombre} ${this.sector}`;
+    }
+};
+console.log(empresa2.mostrar());
--- typeScriptModerno/src/05Objetos.ts
+++ typeScriptModerno/src/05Objetos.ts
@@ -1,3 +1,5 @@
+      // ? Creando objetos en TS
+
 let empresa = { 
   nombre: 'MICROSOFT INC',
   sector: 'Informatica',
@@ -9,3 +11,30 @@ let empresa = {
 //   nombre: 1234,      // todo: Esto no se puede hacer porque 1, no se respetal el tipo de valor y 2 no se respeta el 'key' del segundo valor
 //   categoria: 'hola'
 // }
+
+
+
+      // ? Declarando tipos de datos en Objectos:
+
+// type empresas es el alias para los tipos de valores.
+type empresas = {
+  nombre: string,
+  sector: string,
+  servicios: string[], // (array de strings)
+  facturacion: number,
+
+  mostrar: () => string  // Asi se declara las variables que retornaran un valor de tipo string.
+}
+
+let empresa2: empresas = { // Aqui se introducen los valores
+  nombre: 'MICROSOFT INC',
+  sector: 'Informatica',
+  servicios: ['sistemas operativos', 'ofimatica', 'consolas'],
+  facturacion: 55_000_000_000_000,
+
+  mostrar() { 
+    return `${this.nombre} ${this.sector}`;
+  }
+}
+
+console.log(empresa2.mostrar());
