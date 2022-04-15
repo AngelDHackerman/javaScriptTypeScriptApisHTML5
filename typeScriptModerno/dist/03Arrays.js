122cd6d1cdddadc3309856450740c5b953494bf3
angelDariaux <angeldariaux@gmail.com>
2022-04-15 11:29:04 -0600

    creando arrays

---
+++ typeScriptModerno/dist/03Arrays.js
@@ -0,0 +1,10 @@
+"use strict";
+// ? Arrays 
+// * Array de solo strings
+let personas = ['Vicotr', 'Paco', 'Ange'];
+personas.push('Oscar');
+console.log(personas);
+// * Array de solo numeros
+let years = [2000, 1998, 1996, 2022];
+years.push(2025);
+console.log(years);
---
+++ typeScriptModerno/src/03Arrays.ts
@@ -0,0 +1,16 @@
+      // ? Arrays 
+
+      // * Array de solo strings
+
+      let personas:string[] = ['Vicotr', 'Paco', 'Ange'];
+
+personas.push('Oscar');
+console.log(personas)
+
+
+      // * Array de solo numeros
+
+let years:number[] = [2000, 1998, 1996, 2022];
+
+years.push(2025);
+console.log(years);
