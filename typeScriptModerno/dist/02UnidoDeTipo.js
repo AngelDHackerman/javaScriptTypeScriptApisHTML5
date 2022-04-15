3f0476d683a6d097331789e8e9f69f39340430b2
angelDariaux <angeldariaux@gmail.com>
2022-04-15 11:22:48 -0600

    union de tipos

---
+++ typeScriptModerno/dist/02UnidoDeTipo.js
@@ -0,0 +1,4 @@
+"use strict";
+// ? union de tipos 
+let contacto = 'Angel H.';
+contacto = 123456789;
---
+++ typeScriptModerno/src/02UnidoDeTipo.ts
@@ -0,0 +1,5 @@
+      // ? union de tipos 
+
+let contacto: string | number = 'Angel H.'; 
+
+contacto = 123_456_789;
