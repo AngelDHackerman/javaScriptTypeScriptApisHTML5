13e59ac7cbc91240badd6674ab85b86bb7a16685
angelDariaux <angeldariaux@gmail.com>
2022-04-15 11:43:36 -0600

    creando tuplas y tipo enum

--- typeScriptModerno/dist/03Arrays.js
+++ typeScriptModerno/dist/03Arrays.js
@@ -1,10 +1,34 @@
 "use strict";
 // ? Arrays 
 // * Array de solo strings
+console.group('Array de solo strings: ');
 let personas = ['Vicotr', 'Paco', 'Ange'];
 personas.push('Oscar');
 console.log(personas);
+console.groupEnd();
 // * Array de solo numeros
+console.group('Array de solo numeros: ');
 let years = [2000, 1998, 1996, 2022];
 years.push(2025);
 console.log(years);
+console.groupEnd();
+// ? Tuplas, son arryas con tipos ya definidos y una longitud ya definida
+//  No puede haber ni mas ni menos elementos de los ya definidos.
+console.group('Tuplas: ');
+let pelicula = ['Bartamen begins', true, 1990];
+console.log(typeof pelicula);
+console.groupEnd();
+// ? enum - enumeracion, dentro del enum NO se puede declara el tipo de dato.
+console.group('Enum - enumeracion: ');
+var datosPersonales;
+(function (datosPersonales) {
+    datosPersonales["nombre"] = "Angel";
+    datosPersonales[datosPersonales["dpi"] = 22299338380128] = "dpi";
+    datosPersonales["direccion"] = "Calle de las fatasias";
+    datosPersonales["ciudad"] = "Xelaju";
+})(datosPersonales || (datosPersonales = {}));
+// datosPersonales.nombre = 'Carlos';        // * Esto no funciona, NO se pueden cambiar los valores
+console.log(datosPersonales.nombre, datosPersonales.dpi, datosPersonales.direccion, datosPersonales.ciudad);
+console.groupEnd();
+console.group();
+console.groupEnd();
--- typeScriptModerno/src/03Arrays.ts
+++ typeScriptModerno/src/03Arrays.ts
@@ -1,16 +1,74 @@
       // ? Arrays 
 
-      // * Array de solo strings
+            // * Array de solo strings
+console.group('Array de solo strings: ');
 
       let personas:string[] = ['Vicotr', 'Paco', 'Ange'];
 
 personas.push('Oscar');
 console.log(personas)
 
+console.groupEnd();
 
-      // * Array de solo numeros
+
+
+            // * Array de solo numeros
+
+console.group('Array de solo numeros: ');
 
 let years:number[] = [2000, 1998, 1996, 2022];
 
 years.push(2025);
 console.log(years);
+
+console.groupEnd();
+
+
+
+      // ? Tuplas, son arryas con tipos ya definidos y una longitud ya definida
+      //  No puede haber ni mas ni menos elementos de los ya definidos.
+
+console.group('Tuplas: ');
+
+let pelicula: [string, boolean, number] = ['Bartamen begins', true, 1990];
+
+console.log(typeof pelicula);
+
+console.groupEnd();
+
+
+
+      // ? enum - enumeracion, dentro del enum NO se puede declara el tipo de dato.
+
+console.group('Enum - enumeracion: ');
+
+enum datosPersonales { 
+      nombre = 'Angel',
+      dpi = 222_993_3838_0128,
+      direccion = 'Calle de las fatasias',
+      ciudad = 'Xelaju'
+}
+
+// datosPersonales.nombre = 'Carlos';        // * Esto no funciona, NO se pueden cambiar los valores
+
+console.log(datosPersonales.nombre, datosPersonales.dpi, datosPersonales.direccion, datosPersonales.ciudad);
+
+console.groupEnd();      
+
+
+
+
+
+
+
+
+
+
+
+console.group();
+
+
+
+console.groupEnd();
+
+
