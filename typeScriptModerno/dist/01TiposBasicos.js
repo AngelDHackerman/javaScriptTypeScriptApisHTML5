6dbac7c868ee9192ffcda2befae03e906e86c83e
angelDariaux <angeldariaux@gmail.com>
2022-04-14 22:36:37 -0600

    tipos de datos basicos en TS

--- typeScriptModerno/dist/01TiposBasicos.js
+++ typeScriptModerno/dist/01TiposBasicos.js
@@ -1,1 +1,15 @@
 "use strict";
+// Tipos de datos basicos 
+// String
+let texto = 'Hola que tal!';
+// Number
+let numero = 10;
+numero = 11.25;
+console.log(numero);
+// boolean
+let verdadero = true;
+// Any, el NO recomendado.
+let normal = 'Cualquier cosa';
+normal = true;
+normal = 17;
+console.log(normal);
---
+++ typeScriptModerno/src/01TiposBasicos.ts
@@ -0,0 +1,31 @@
+// Tipos de datos basicos 
+
+      // String
+
+let texto:string = 'Hola que tal!';
+
+
+      // Number
+
+let numero:number = 10;
+numero = 11.25
+
+console.log(numero)
+
+
+      // boolean
+
+let verdadero:boolean = true;
+
+ 
+      // Any, el NO recomendado.
+
+let normal:any = 'Cualquier cosa';
+normal = true;
+normal = 17;
+
+console.log(normal);
+
+
+
+
