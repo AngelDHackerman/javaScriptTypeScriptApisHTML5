2f5fcf7513673662605bc0690f36f172034a1af0
angelDariaux <angeldariaux@gmail.com>
2022-04-16 23:26:49 -0600

    iniciamos con APIs de HTML

---
+++ apisHTML5/01LocalStorage/js/main.js
@@ -0,0 +1,12 @@
+      // ? Comprobar si el navegador es compatible 
+
+if (typeof(Storage) !== 'undefined') { 
+  console.log('LocalStorage disponible');
+} else { 
+  console.log('LocalStorage NO DISPONIBLE')
+}
+
+
+
+      // ? Guardar datos en el navegador 
+
